b7289afc59c79abc3b9430f7180dac3b78047bd5
Spring Wong <spring@Springs-MacBook-Air.local>
2018-03-10 23:29:16 +0800

    First commit Remove storyboard

---
+++ SwiftMVVMSample/SimpleViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SimpleViewController.swift
+//  SwiftMVVMSample
+//
+//  Created by Spring Wong on 10/3/2018.
+//  Copyright © 2018 Spring Wong. All rights reserved.
+//
+
+import UIKit
+
+class SimpleViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
